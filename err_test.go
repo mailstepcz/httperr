c41f1ad7def2d080530cec3c970ecb70f0ad86b5
Petr Homola <homola.p@gmail.com>
2024-11-21 10:56:24 +0000

    initial release (#1)
    * initial release
    
    * using serr attrs

---
+++ err_test.go
@@ -0,0 +1,46 @@
+package httperr
+
+import (
+	"errors"
+	"net/http"
+	"testing"
+
+	"github.com/mailstepcz/serr"
+	"github.com/stretchr/testify/require"
+)
+
+func TestHTTPStatus(t *testing.T) {
+	req := require.New(t)
+
+	dummyErr := Wrap(errors.ErrUnsupported, http.StatusNotImplemented)
+
+	req.Equal(http.StatusNotImplemented, HTTPStatus(dummyErr))
+
+	req.Equal("unsupported operation", dummyErr.Error())
+
+	req.True(errors.Is(dummyErr, errors.ErrUnsupported))
+}
+
+func TestHTTPWrappedError(t *testing.T) {
+	req := require.New(t)
+
+	dummyErr := serr.Wrap("wrapped", Wrap(errors.ErrUnsupported, http.StatusNotImplemented))
+
+	req.Equal(http.StatusNotImplemented, HTTPStatus(dummyErr))
+
+	req.Equal("wrapped: unsupported operation", dummyErr.Error())
+
+	req.True(errors.Is(dummyErr, errors.ErrUnsupported))
+}
+
+func TestHTTPWrappedErrors(t *testing.T) {
+	req := require.New(t)
+
+	dummyErr := errors.Join(errors.New("some error"), serr.Wrap("wrapped", Wrap(errors.ErrUnsupported, http.StatusNotImplemented)))
+
+	req.Equal(http.StatusNotImplemented, HTTPStatus(dummyErr))
+
+	req.Equal("some error\nwrapped: unsupported operation", dummyErr.Error())
+
+	req.True(errors.Is(dummyErr, errors.ErrUnsupported))
+}
---
+++ go.mod
@@ -0,0 +1,21 @@
+module github.com/mailstepcz/httperr
+
+go 1.22.7
+
+toolchain go1.23.3
+
+require (
+	github.com/mailstepcz/serr v0.1.2
+	github.com/stretchr/testify v1.9.0
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	golang.org/x/sys v0.27.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20241118233622-e639e219e697 // indirect
+	google.golang.org/grpc v1.68.0 // indirect
+	google.golang.org/protobuf v1.35.2 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
